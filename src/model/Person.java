fbbb6720612eae7268907032441230f7229a6132
vimasha <vimashapalihawadana10@gmail.com>
2026-07-14 01:47:12 +0530

    Add Person class

--- src/model/Person.java
+++ src/model/Person.java
@@ -1,4 +1,59 @@
 package model;
 
-public class Person {
+public abstract class Person {
+
+    private String id;
+    private String name;
+    private int age;
+    private String email;
+
+    public Person() {
+
+    }
+
+    public Person(String id, String name, int age, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    // Getters
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // Setters
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public abstract void displayInfo();
+
 }
