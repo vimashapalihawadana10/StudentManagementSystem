b57fa8f4fb64dc10236a76febba0137b1be37fde
vimasha <vimashapalihawadana10@gmail.com>
2026-07-14 02:13:14 +0530

    Add Enrollment class

--- src/model/Enrollment.java
+++ src/model/Enrollment.java
@@ -1,4 +1,31 @@
 package model;
 
 public class Enrollment {
+
+    private Student student;
+    private Course course;
+
+    public Enrollment(Student student, Course course) {
+
+        this.student = student;
+        this.course = course;
+
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void displayEnrollment() {
+
+        System.out.println("------------------------");
+        System.out.println("Student : " + student.getName());
+        System.out.println("Course  : " + course.getCourseName());
+
+    }
+
 }
