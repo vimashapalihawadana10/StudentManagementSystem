c62adbac67e21207facdde3caa7d1bdee61a27cb
vimasha <vimashapalihawadana10@gmail.com>
2026-07-14 01:51:59 +0530

    Add Teacher class

--- src/model/Teacher.java
+++ src/model/Teacher.java
@@ -1,4 +1,38 @@
 package model;
 
-public class Teacher {
+public class Teacher extends Person {
+
+    private String department;
+
+    public Teacher() {
+
+    }
+
+    public Teacher(String id, String name, int age, String email, String department) {
+
+        super(id, name, age, email);
+        this.department = department;
+
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public void displayInfo() {
+
+        System.out.println("------------------------");
+        System.out.println("Teacher ID : " + getId());
+        System.out.println("Name       : " + getName());
+        System.out.println("Age        : " + getAge());
+        System.out.println("Email      : " + getEmail());
+        System.out.println("Department : " + department);
+
+    }
+
 }
