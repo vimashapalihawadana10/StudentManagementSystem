094832e8f12490d2a2c369114cf89a950d3ca901
vimasha <vimashapalihawadana10@gmail.com>
2026-07-14 01:49:32 +0530

    Add Student class

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,4 +1,23 @@
 package model;
 
-public class Student {
+public class Student extends Person {
+
+    public Student() {
+
+    }
+
+    public Student(String id, String name, int age, String email) {
+        super(id, name, age, email);
+    }
+
+    @Override
+    public void displayInfo() {
+
+        System.out.println("------------------------");
+        System.out.println("Student ID : " + getId());
+        System.out.println("Name       : " + getName());
+        System.out.println("Age        : " + getAge());
+        System.out.println("Email      : " + getEmail());
+    }
+
 }
