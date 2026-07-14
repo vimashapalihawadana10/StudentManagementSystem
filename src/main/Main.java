9a258c3dacc46f79bfd469758a908affdf1ab480
vimasha <vimashapalihawadana10@gmail.com>
2026-07-14 14:58:40 +0530

    complete version A

--- src/main/Main.java
+++ src/main/Main.java
@@ -74,7 +74,7 @@ public class Main {
         do {
 
             System.out.println("\n===== Student Menu =====");
-            System.out.println("1. Add Student");
+            System.out.println("1. Add  Student");
             System.out.println("2. View Students");
             System.out.println("3. Search Student");
             System.out.println("4. Update Student");
